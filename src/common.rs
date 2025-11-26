d46268a038c64a0f2da69735aa22f1d58c233f93
zzzw4889 <zzz1zzz@163.com>
2025-11-26 14:55:57 +0800

    更新配置文件

--- src/common.rs
+++ src/common.rs
@@ -1046,7 +1046,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://192.168.168.130".to_owned()
 }
 
 #[inline]
